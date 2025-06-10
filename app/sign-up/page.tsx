c45202fc07917c276931972088f09a8ee25f8385
Darcy Liu <darcy.ldx@gmail.com>
2025-06-10 16:24:08 -0400

    )}

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -13,7 +13,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Navbar } from "@/components/navbar";
 import { EyeIcon, EyeOffIcon, CheckCircleIcon } from "lucide-react";
 import { useAuth } from "@/contexts/auth-context";
-import { CircleLoader } from "@/components/loading-screen";
+import { LoadingScreen } from "@/components/loading-screen";
 
 export default function SignUp() {
   const [name, setName] = useState("");
@@ -91,11 +91,10 @@ export default function SignUp() {
 
   return (
     <div className="min-h-screen bg-black text-white">
+      {showLoadingScreen && <LoadingScreen />}
       {!showLoadingScreen && <Navbar />}
-
+      {!showLoadingScreen && (
       <main className="container mx-auto px-6 pt-24 pb-16 flex justify-center">
-        {showLoadingScreen && <CircleLoader />}
-        {!showLoadingScreen && (
           <div className="w-full max-w-md">
             <div className="text-center mb-8">
               <Image
@@ -309,8 +308,8 @@ export default function SignUp() {
               </div>
             </form>
           </div>
-        )}
-      </main>
+       
+      </main> )}
 
       <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-gray-950 to-transparent -z-10"></div>
     </div>
